a964fbd44aa791984f827772d0e482184adcd700
Ubaid-Stack <ubaidullahfareed2003@gmail.com>
2026-06-28 16:55:44 +0530

    Move RAWG API key to environment variables

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "ff3f1096f3784ccbbefe1e74d51c6f17",
+    key: import.meta.env.VITE_RAWG_API_KEY,
   },
 });
