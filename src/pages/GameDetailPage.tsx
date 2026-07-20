2f0a9ceba68bae632cbd8e81fa651888a2783b2c
Ubaid-Stack <ubaidullahfareed2003@gmail.com>
2026-07-20 06:59:10 +0530

    Build expandable text component

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,5 +1,6 @@
+import ExpandableText from "../components/ExpandableText";
 import useGames from "../hooks/useGames";
-import { Heading, Spinner, Text } from "@chakra-ui/react";
+import { Heading, Spinner } from "@chakra-ui/react";
 import { useParams } from "react-router";
 
 const GameDetailPage = () => {
@@ -12,7 +13,7 @@ const GameDetailPage = () => {
   return (
     <>
       <Heading>{game.name}</Heading>
-      <Text>{game.description_raw}</Text>
+      <ExpandableText>{game.description_raw}</ExpandableText>
     </>
   );
 };
